07d7e54c871a948fc61e1ecf91c7f481f13e40b7
Gethin Jones <gethin@Gethins-MacBook-Pro.local>
2025-11-27 19:34:52 +0000

    i want snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      explorer = { enabled = false }, -- turn off the built-in explorer
+      explorer = { enabled = true }, -- turn off the built-in explorer
     },
   },
 }
